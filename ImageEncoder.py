4fbb61dd9dab503f50390a8d2c5b1463dfc35084
Liam Kreiss <liam.kreiss@gmail.com>
2020-02-28 16:26:41 -0800

    got rid of print statements

--- ImageEncoder.py
+++ ImageEncoder.py
@@ -5,13 +5,13 @@ convertStringToInt = {"00": 0, "01": 1, "10": 2, "11": 3}
 convertIntToString = {0: "00", 1: '01', 2: '10', 3: '11'}
 
 #Creates image
-im = Image.new("RGB", (1024,1024))
+im = Image.open("IMG_4056-4.jpeg")
 pixelsNew = im.load()
-for i in range(im.size[0]):
-    for j in range(im.size[1]):
-        pixelsNew[i, j] = (0, 255, 0, 255)
+# for i in range(im.size[0]):
+#     for j in range(im.size[1]):
+#         pixelsNew[i, j] = (0, 255, 0, 255)
 
-messageToInsert = "TUNA Needs to stop running!!!!TUNA Needs to stop running!!!!TUNA Needs to stop running!!!!"
+messageToInsert = "Hi Mason! This is secret haha"
  # Needs to stop running!!!!TUNA Needs to stop running!!!!TUNA Needs to stop running!!!!"
 
 def string2bits(s):
@@ -27,7 +27,7 @@ def convertMessageToBinary(stringToConvert):
     # res = ''.join(format(ord(i), 'b') for i in stringToConvert)
     # res = bin(int.from_bytes(stringToConvert.encode(), 'big'))[2:] #cut off the 0b part
     res = string2bits(stringToConvert)
-    print("message converted into binary:", res)
+    # print("message converted into binary:", res)
     return res
 
 def getNewRGBValue(initialVal, res, i):
@@ -46,24 +46,24 @@ def insertMessage(binaryMessage):
     #Iterate through each bit in Message
     for i in range(math.ceil(len(binaryMessage)/6)):
         colorR = pixelsNew[0, i][0]
-        print(colorR, "colorR")
+        # print(colorR, "colorR")
         colorG = pixelsNew[0, i][1]
-        print(colorG, "colorG")
+        # print(colorG, "colorG")
         colorB = pixelsNew[0, i][2]
-        print(colorB, "colorB")
+        # print(colorB, "colorB")
 
         #Assign RGB values
         newR = getNewRGBValue(colorR, binaryMessage, (i * 6))
-        print(newR, "newR")
+        # print(newR, "newR")
         newG = getNewRGBValue(colorG, binaryMessage, (i * 6) + 2)
-        print(newG, "newG")
+        # print(newG, "newG")
         newB = getNewRGBValue(colorB, binaryMessage, (i * 6) + 4)
 
         pixelsNew[0, i] = (newR, newG, newB, 255)
 
-        print(newB, "newB")
+        # print(newB, "newB")
         print(binaryMessage[i*6:(i+1)*6])
-        print('')
+        # print('')
 
 
 def decryptMessage(pixelArray):
@@ -72,7 +72,7 @@ def decryptMessage(pixelArray):
     for i in range(200): #should be 1024 for this file, but I truncated
         for j in range(3):
             binaryString += convertIntToString[pixelArray[0, i][j] % 4]
-    print(binaryString)
+    # print(binaryString)
 
     # n = int('0b' + binaryString, 2)
     # print("decryptMessage:", n.to_bytes((n.bit_length() + 7) // 8, 'big').decode())
